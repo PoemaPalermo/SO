7e561245f5edbfebbe07fcf82eb5ae4f92a1553a
Lucia Poema Palermo <163904623+PoemaPalermo@users.noreply.github.com>
2025-07-02 10:10:01 -0300

    Cambios por error de printeo de batch 3

--- Batch3.bat
+++ Batch3.bat
@@ -2,11 +2,11 @@
 :inicio
 cls
 echo Bienvenido al Quiz!
-set /p respuesta1="Pregunta 1: ¿Cuánto es 2+2? "
+set /p respuesta1="Pregunta 1: Cuanto es 2+2? "
 if "%respuesta1%"=="4" (
-    set /p respuesta2="Pregunta 2: ¿Cuánto es 5-3? "
+    set /p respuesta2="Pregunta 2: Cuanto es 5-3? "
     if "%respuesta2%"=="2" (
-        set /p respuesta3="Pregunta 3: ¿Cuánto es 2*8? "
+        set /p respuesta3="Pregunta 3: Cuanto es 2*8? "
         if "%respuesta3%"=="16" (
             echo Lo lograste!
             pause
