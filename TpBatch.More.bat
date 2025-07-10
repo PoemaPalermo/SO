556d64e05ec217d306ab71098113b4702a963b09
MorenaLopezObejero <48111548@est.ort.edu.ar>
2025-07-10 11:43:23 -0300

    Update TP.batch more

--- TpBatch.More.bat
+++ TpBatch.More.bat
@@ -1,39 +1,53 @@
 @echo off
-:inicio
+setlocal enabledelayedexpansion
 cls
-echo Bienvenido alTrabajo Práctico Integrador de Batch!
+
+:inicio
+echo.
+echo Bienvenido
+echo --- MENU ---
 echo 1. Sumar
 echo 2. Restar
 echo 3. Multiplicar
-echo 4. Contar con for
+echo 4. Contar
 echo 5. Salir
+set /p opcion=Elija una opcion (1-5):
+echo.
 
-set /p opcion=Opción elegida:  
 
+if "%opcion%"=="1" goto opcion_1
+if "%opcion%"=="2" goto opcion_2
+if "%opcion%"=="3" goto opcion_3
+if "%opcion%"=="4" goto opcion_4
+if "%opcion%"=="5" exit
 
-IF %opcion% == 1 (
-set /p numero=ingresá tu nombre:
-echo %numero%
-pause
-)
+:opcion_1
+echo Genial ahora elija dos numeros:
+set /p num1="Ingrese el primer numero: "
+set /p num2="Ingrese el segundo numero: "
+set /a resultado=%num1%+%num2%
+echo El resultado de la suma es: %resultado%
+goto inicio
 
-@REM if %opcion% == 1 (
-@REM     set /p num1=primero
-@REM     set /p num2=segundo
-@REM     set /a resultado=%num1% + %num2%
-@REM     echo el resultado es %resultado%
-@REM ) 
+:opcion_2
+echo Genial ahora elija dos numeros:
+set /p num1="Ingrese el primer numero: "
+set /p num2="Ingrese el segundo numero: "
+set /a resultado=%num1%-%num2%
+echo El resultado de la resta es: %resultado%
+goto inicio
 
-pause
+:opcion_3
+echo Genial ahora elija dos numeros:
+set /p num1="Ingrese el primer numero: "
+set /p num2="Ingrese el segundo numero: "
+set /a resultado= %num1%*%num2%
+echo El resultado de la multiplicacion es: %resultado%
+goto inicio
 
-@REM else if "%opcion%" == "2" (
-@REM     set /p num1 = "Ahora elegi el primer numero: "
-@REM     set /p num2 = "Ahora elegi el segundo numero: "
-@REM     set /a resultado=num1-num2
-@REM     echo el resultado es %resultado%
-@REM ) else if "%opcion%" == "3" (
-@REM     set /p num1 = "Ahora elegi el primer numero: "
-@REM     set /p num2 = "Ahora elegi el segundo numero: "
-@REM     set /a resultado=num1-num2
-@REM     echo el resultado es %resultado%
-@REM )
+:opcion_4
+set /p num1="Ingresa el numero menor: "
+set /p num2="Ingresa el numero mayor: "
+echo Contando:
+for /L %%i in (%num1%,1,%num2%) DO echo resultado = %%i
+goto inicio
